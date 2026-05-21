2575abdb513667eb37e83039dc4ea0edbb63cb9f
Aferokript <alberto.cardinale@bk.ru>
2026-05-21 16:29:02 +0300

    Добавил вывод блюда

--- Шаблон.py
+++ Шаблон.py
@@ -14,30 +14,68 @@ cooking_menu = [
             {"name": "Имя ингредиента", "price": 0},
             {"name": "Имя ингредиента", "price": 0}
         ],
-        "Общая стоимость": 0,
-        "Инструкция приготовления": '',
+        "price": 0,
+        "instructions": '',
     },
 ]
-
-managers_id = {7280963930} # мужики не путайтесь, это такое множество
 
 token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
 
-managers_menu = ReplyKeyboardMarkup()
+managers_menu = ReplyKeyboardMarkup(resize_keyboard=True)
 managers_menu.row('Добавить блюдо', 'Удалить блюдо')
 
-users_main_menu = ReplyKeyboardMarkup()
+users_main_menu = ReplyKeyboardMarkup(resize_keyboard=True)
 users_main_menu.row('Посмотреть рецепт', 'Добавить в избранное', 'Купить бесконечные просмотры')
+
+managers_id = {7280963930} # мужики не путайтесь, это такое множество
+user_free_dishes = {}
+
+
+def get_random_dish():
+    if not cooking_menu:
+        return None
+    return random.choice(cooking_menu)
+
+
+def get_formatted_dishes(dishes):
+    for dish in dishes:
+        photo = dish.get('photo')
+        name = dish.get('name')
+        ingredients = dish.get('ingredients')
+        price = dish.get('price')
+        instructions = dish.get('instructions')
+        return (f'{photo}'
+                f' Название {name}'
+                f' Список ингредиентов: {ingredients},'
+                f' Общая стоимость: {price},'
+                f' Инструкция приготовления: {instructions}')
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    user_id = message.from_user.id
+    user_id = message.chat.id
     if user_id in managers_id:
         bot.send_message(message.chat.id, 'С возвращением, администратор', reply_markup=managers_menu)
     else:
         bot.send_message(message.chat.id, 'Выбирайте уникальные меню на ваш выбор!', reply_markup=users_main_menu)
 
 
+@bot.message_handler(func=lambda message: message.text == 'Посмотреть рецепт')
+def show_dishes(message):
+    user_id = message.chat.id
+    random_dish = get_random_dish()
+
+    if user_id not in user_free_dishes:
+        user_free_dishes[user_id] = 3
+
+    if user_free_dishes[user_id] > 0:
+        bot.send_message(message.chat.id, get_formatted_dishes([random_dish]), reply_markup=users_main_menu)
+        user_free_dishes[user_id] -= 1
+
+    else:
+        bot.send_message(message.chat.id, 'Ваши бесплатные просмотры закончились! Купите подписку')
+
+
+bot.polling()
 bot.polling()
